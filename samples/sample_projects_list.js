ad7943064a9116a38f41b0a884ffa621e25dc152
normroatest <104795857+normroatest@users.noreply.github.com>
2022-05-04 15:19:15 -0700

    Add samples
    Samples added to the project

---
+++ samples/sample_projects_list.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "id": 7442260,
+  "name": "Norm's first project",
+  "mediaCount": 1,
+  "created": "2022-05-02T22:20:52+00:00",
+  "updated": "2022-05-02T22:22:36+00:00",
+  "hashedId": "z5epmk6uor",
+  "anonymousCanUpload": false,
+  "anonymousCanDownload": false,
+  "public": true,
+  "publicId": "z5epmk6uor",
+  "description": "Get started by adding a video to your project - you can always delete it later!"
+}
